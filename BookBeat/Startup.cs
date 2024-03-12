e781fefabe2c1b7943e00e53cc9bc4636c54bab8
codeyumm <pvashi006@gmail.com>
2024-03-12 13:04:47 -0400

    Add project files.

---
+++ BookBeat/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BookBeat.Startup))]
+namespace BookBeat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
