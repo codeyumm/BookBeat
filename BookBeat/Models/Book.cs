fb7c502bd5ca61a370418fb0a0bbfa8e282a749e
codeyumm <pvashi006@gmail.com>
2024-03-25 21:45:32 -0400

    Added model for Book and Track

---
+++ BookBeat/Models/Book.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookBeat.Models
+{
+    public class Book
+    {
+        [Key]
+        public int BookID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Genre { get; set; }
+
+        // ? to make nullable field for int
+        public int? PublicationYear { get; set; }
+
+        public string ISBN { get; set; }
+
+        public string Description { get; set; }
+
+        public string CoverImageURL { get; set; }
+
+        // Review can have many book
+        // TODO
+
+        // MediaList can have many book
+        // TODO
+    }
+
+    // DTO object to abstract some data
+    public class BookDTO
+    {
+        public int BookID { get; set;}
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+    
+    }
+}
---
+++ BookBeat/Models/Track.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BookBeat.Models
+{
+    public class Track
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Album { get; set; }
+
+        public string AlbumArt { get; set; }
+
+        public DateTime? ReleaseDate { get; set; } = new DateTime(2024, 12, 12);
+
+        // Reviews can have many tracks
+        // public ICollection<TrackList> TrackLists { get; set; }
+
+        // TrackList can have many tracks
+        // TODO
+    }
+
+    // DTO Object to abstract some data
+    public class TrackDTO
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Artist { get; set; }
+
+        public string Album { get; set;}
+    }
+
+
+}
