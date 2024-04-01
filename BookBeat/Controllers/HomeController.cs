33e2ef4130f73b8e3d5b540c80c8fb7659a7fd77
codeyumm <pvashi006@gmail.com>
2024-03-31 22:32:40 -0400

    TrackSearch books are not showing

--- BookBeat/Controllers/HomeController.cs
+++ BookBeat/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 ﻿using BookBeat.Models;
-using BookBeat.Models.VIewModels;
+using BookBeat.Models.ViewModels;
 using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
@@ -77,102 +77,145 @@ namespace BookBeat.Controllers
             // empty book list
             List<Book> books = new List<Book>();
 
-
-            HttpRequestMessage bookRequest = new HttpRequestMessage
+            // Fetch 10 random books
+            for (int i = 0; i < 10; i++)
             {
-                Method = HttpMethod.Get,
-                RequestUri = new Uri(bookUrl),
-                Headers =
+                HttpRequestMessage bookRequest = new HttpRequestMessage
+                {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri(bookUrl),
+                    Headers =
         {
             { "X-RapidAPI-Key", "92cd5fcfa4mshd97d398e5b8c2d3p1b650cjsn6c18a16505b8" },
             { "X-RapidAPI-Host", "books-api7.p.rapidapi.com" },
         },
-            };
+                };
 
-            HttpResponseMessage bookResponse = await client.SendAsync(bookRequest);
+                HttpResponseMessage bookResponse = await client.SendAsync(bookRequest);
 
-            if (bookResponse.IsSuccessStatusCode)
-            {
-                Debug.WriteLine("Book response success");
-                var bookJson = await bookResponse.Content.ReadAsStringAsync();
-                JObject bookData = JObject.Parse(bookJson);
-
-                // Extract book information and add to book model
-                string title = bookData["title"].ToString();
-                string author = bookData["author"].ToString();
-                string coverUrl = bookData["cover"].ToString();
-
-                books.Add(new Book
+                if (bookResponse.IsSuccessStatusCode)
                 {
-                    Title = title,
-                    Author = author,
-                    CoverImageURL = coverUrl
+                    Debug.WriteLine($"Book response success {i + 1}");
 
-                });
-               
+                    var bookJson = await bookResponse.Content.ReadAsStringAsync();
+                    JObject bookData = JObject.Parse(bookJson);
+
+                    // Extract book information and add to book model
+                    string title = bookData["title"].ToString();
+                    string author = bookData["author"]["first_name"].ToString() + " " + bookData["author"]["last_name"].ToString();
+                    string coverUrl = bookData["cover"].ToString();
+
+                    books.Add(new Book
+                    {
+                        Title = title,
+                        Author = author,
+                        CoverImageURL = coverUrl
+                    });
+                }
             }
 
             // set book model to viewmodel
+            ViewModel.books = books;
+
 
             
-            return View(tracksModel);
+            return View(ViewModel);
         }
 
 
         // GET - localhost:44387/home/tracksearch
         // to handle user search for a track
+
         public async Task<ActionResult> TrackSearch(string searchQuery)
         {
-
-            // get the search string from request
+           
             string searchString = searchQuery;
 
-            // client object to make request
+            
             HttpClient client = new HttpClient();
 
-            // api url
-            string url = $"https://api.deezer.com/search?q={searchString}";
-            Debug.WriteLine(url);
+            // API URL for music search
+            string musicUrl = $"https://api.deezer.com/search?q={searchString}";
 
-            // empty list of track
-            List<Track> trackList = new List<Track> { };
+            // API URL for book search
+            string bookUrl = $"https://books-api7.p.rapidapi.com/books/find/title?title={searchString}";
 
-            // get response after making the request with api url
-            HttpResponseMessage response = client.GetAsync(url).Result;
+            // Empty list of tracks
+            List<Track> tracks = new List<Track>();
 
-            // check if response was succesful or not
-            if (response.IsSuccessStatusCode)
+            // Empty list of books
+            List<Book> books = new List<Book>();
+
+           
+            HttpResponseMessage musicResponse = await client.GetAsync(musicUrl);
+
+            // Check if music response was successful
+            if (musicResponse.IsSuccessStatusCode)
             {
+                // Store data as string
+                var musicJson = await musicResponse.Content.ReadAsStringAsync();
 
-                // store data as string
-                var json = await response.Content.ReadAsStringAsync();
+                // Convert string into JSON
+                JObject musicData = JObject.Parse(musicJson);
 
-                // convert string into json
-                JObject jsonData = JObject.Parse(json);
+                // Get data which is inside the JSON array
+                JArray musicTracks = (JArray)musicData["data"];
 
-                // get data which is inside the json array
-                JArray tracks = (JArray)jsonData["data"];
-
-                foreach (var track in tracks)
+                foreach (var trackItem in musicTracks)
                 {
+                    // Extract track information and add to track list
+                    string title = trackItem["title"].ToString();
+                    string artist = trackItem["artist"]["name"].ToString();
+                    string albumArt = trackItem["album"]["cover_big"].ToString();
 
-
-                    trackList.Add(new Track
+                    tracks.Add(new Track
                     {
-                        Title = track["title"].ToString(),
-                        Artist = track["artist"]["name"].ToString(),
-                        AlbumArt = track["album"]["cover_big"].ToString()
-
+                        Title = title,
+                        Artist = artist,
+                        AlbumArt = albumArt
                     });
-
-
                 }
-
-
             }
 
-            return View(trackList);
+           
+            HttpResponseMessage bookResponse = await client.GetAsync(bookUrl);
+
+            
+            if (bookResponse.IsSuccessStatusCode)
+            {
+             
+                var bookJson = await bookResponse.Content.ReadAsStringAsync();
+
+                
+                JObject bookData = JObject.Parse(bookJson);
+                JArray bookArray = (JArray)bookData["data"];
+
+                foreach (var bookItem in bookArray)
+                {
+                    // get book info to store in model
+                    string title = bookItem["title"].ToString();
+                    string author = bookItem["author"]["first_name"].ToString() + " " + bookItem["author"]["last_name"].ToString();
+                    string coverUrl = bookItem["cover"].ToString();
+
+                    books.Add(new Book
+                    {
+                        Title = title,
+                        Author = author,
+                        CoverImageURL = coverUrl
+                    });
+                }
+            }
+
+            // set view models
+            HomeMedia viewModel = new HomeMedia
+            {
+                tracks = tracks,
+                books = books
+            };
+
+            return View(viewModel);
         }
+
 
 
     }
